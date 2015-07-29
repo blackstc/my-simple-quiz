caea32e58ca782a77a828eb9ed010d00a79caad4
Blackstock <charlieblackstock@gmail.com>
2015-07-29 17:54:34 -0600

    started quiz outline with dynamic question buildling

--- js/main.js
+++ js/main.js
@@ -2,4 +2,24 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
+
+
+
+
+  function createQuestionHeader(header, num) {
+    $('#quizForm').append("<div><h3>" + num + ". " + header + '</h3></div>');
+    for (var i = 0; i < quiz[num - 1].options.length; i++) {
+      $('#quizForm').append(createAnswers(quiz[num - 1].options[i]));
+    }
+  }
+
+  function createAnswers (text, id) {
+    $('#quizForm').append('<label id='+ id +'><input type="radio"/>&nbsp;&nbsp;' + text + '</label><br>');
+  }
+
+
+createQuestionHeader(quiz[0].question, 1);
+createQuestionHeader(quiz[1].question, 2);
 });
+
+
---
+++ js/quiz.js
@@ -0,0 +1,14 @@
+var quiz = [
+  {
+    uniqueID: 1,
+    question: "Who shot first Han Solo or Greedo",
+    options: ["Han solo", "Greedo", "Wedge"],
+    answer: "Han Solo"
+  },
+  {
+    uniqueID: 2,
+    question: "Who is cooler, Kyle or Charlie",
+    options: ["Kyle", "Charlie", "Robby", "Michael"],
+    answer: "Kyle"
+  }
+];
