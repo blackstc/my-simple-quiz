3a42b846b2eeee033435a42cca634af03a62da8d
Blackstock <charlieblackstock@gmail.com>
2015-07-30 15:38:32 -0600

    added answer evalution and added function comments

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
 // add scripts
 
+
 $(document).on('ready', function() {
-  console.log('sanity check!');
 
-
-
-
-  function createQuestionHeader(header, num) {
-    $('#quizForm').append("<div><h3>" + num + ". " + header + '</h3></div>');
+  //input: question text and question number. Process: add question and related options to a single Div.  Out: appends new div of questions and answers to the DOM.
+  function createQuestion(question, num) {
+    $('#quizForm').append('<div id=' + num +'><h3>' + num + ". " + question + '</h3></div>');
     for (var i = 0; i < quiz[num - 1].options.length; i++) {
-      $('#quizForm').append(createAnswers(quiz[num - 1].options[i]));
+      $('#quizForm').append(createAnswers(quiz[num - 1].options[i], num));
     }
   }
 
+  //input: answer option and unique ID.  Process: add the text and unique id to an html input and append to DOM.  Out:  appends new text and ID to the DOM
   function createAnswers (text, id) {
-    $('#quizForm').append('<label id='+ id +'><input type="radio"/>&nbsp;&nbsp;' + text + '</label><br>');
+    $('#' + id).append('<label><input name='+ id +' type="radio"/>' + text + '</label><br>');
+  }
+
+  //input: questions and submited answer.  Process: check to see if the answer is correct.  Out: record whether the selected answer is correct.
+  function  checkAnswer (questionNum) {
+    var $answer = $('#' + questionNum).find('input:checked').parent().text();
+
+    console.log($answer);
+    console.log(quiz[0].answer);
+    if (quiz[questionNum - 1].answer === $answer) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 
-createQuestionHeader(quiz[0].question, 1);
-createQuestionHeader(quiz[1].question, 2);
+createQuestion(quiz[0].question, 1);
+createQuestion(quiz[1].question, 2);
+createQuestion(quiz[2].question, 3);
 });
-
-
--- js/quiz.js
+++ js/quiz.js
@@ -2,13 +2,19 @@ var quiz = [
   {
     uniqueID: 1,
     question: "Who shot first Han Solo or Greedo",
-    options: ["Han solo", "Greedo", "Wedge"],
+    options: ["Han Solo", "Greedo", "Wedge", "Pete"],
     answer: "Han Solo"
   },
   {
     uniqueID: 2,
     question: "Who is cooler, Kyle or Charlie",
     options: ["Kyle", "Charlie", "Robby", "Michael"],
-    answer: "Kyle"
+    answer: "  Kyle"
+  },
+  {
+    uniqueID: 3,
+    question: "What is the name the spaceship captained by Han Solo?",
+    options: ["X-wing", "Y-wing", "Milenium Eagle", "Milenium Falcon"],
+    answer: "  Milenium Falcon"
   }
 ];
