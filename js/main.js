934b63c54e8037f88757fe07f81b40506bce08ce
Blackstock <charlieblackstock@gmail.com>
2015-07-30 18:05:20 -0600

    added more questions and added a generator so you can choose how many questions long you want the quiz to be.

--- js/main.js
+++ js/main.js
@@ -2,23 +2,32 @@
 
 
 $(document).on('ready', function() {
+  $("#score").hide();
+
+  //input: number of questions. Process: take number in number of selected questions and generate question list.  Out: append list of questions to the DOM
+  function generateQuiz(number) {
+    for (var i = 0; i < number; i++) {
+      createQuestion(quiz[i].question, (i + 1));
+    }
+  }
 
   //input: question text and question number. Process: add question and related options to a single Div.  Out: appends new div of questions and answers to the DOM.
   function createQuestion(question, num) {
-    $('#quizForm').append('<div id=' + num +'><h3>' + num + ". " + question + '</h3></div>');
+    $('#quizForm').append('<div id=' + num +'><h4>' + num + ". " + question + '</h4></div>');
     for (var i = 0; i < quiz[num - 1].options.length; i++) {
       $('#quizForm').append(createAnswers(quiz[num - 1].options[i], num));
     }
   }
 
   //input: answer option and unique ID.  Process: add the text and unique id to an html input and append to DOM.  Out:  appends new text and ID to the DOM
-  function createAnswers (text, id) {
+  function createAnswers(text, id) {
     $('#' + id).append('<label><input name='+ id +' type="radio"/>' + text + '</label><br>');
   }
 
   //input: questions and submited answer.  Process: check to see if the answer is correct.  Out: record whether the selected answer is correct.
-  function  checkAnswer (questionNum) {
+  function  checkAnswer(questionNum) {
     var $answer = $('#' + questionNum).find('input:checked').parent().text();
+    var counter = 0;
 
     console.log($answer);
     console.log(quiz[0].answer);
@@ -29,8 +38,29 @@ $(document).on('ready', function() {
     }
   }
 
+  $("#score").on("click", function() {
+    var counter = 0;
+    var $number = +$("#question").val();
+    $(".modal-body").html("");
+    for (var i = 0; i < $number; i++) {
+      if (checkAnswer(i + 1)) {
+        $(".modal-body").append("<p>You got question " + (i + 1) + " correct!</p>");
+        counter++;
+      } else {
+        $(".modal-body").append("<p>You got question " + (i + 1) + " wrong! You suck! Get a life!  Obviously, the correct answer was " + quiz[i].answer + ".</p>");
+      }
+    }
+    var quizPercentage = counter/$number * 100;
+    $(".modal-title").html("Score: " + counter + "/" + $number + " or " + quizPercentage + "%");
+  });
 
-createQuestion(quiz[0].question, 1);
-createQuestion(quiz[1].question, 2);
-createQuestion(quiz[2].question, 3);
+  //generates quiz on click of generate quiz button
+  $("#generate").on("click", function() {
+    $("#score").show();
+    $("#generate").hide();
+    $("#question").hide();
+    var $number = +$("#question").val();
+    console.log($number);
+    generateQuiz($number);
+  });
 });
--- js/quiz.js
+++ js/quiz.js
@@ -2,19 +2,37 @@ var quiz = [
   {
     uniqueID: 1,
     question: "Who shot first Han Solo or Greedo",
-    options: ["Han Solo", "Greedo", "Wedge", "Pete"],
+    options: ["Han Solo", "Greedo"],
     answer: "Han Solo"
   },
   {
     uniqueID: 2,
-    question: "Who is cooler, Kyle or Charlie",
-    options: ["Kyle", "Charlie", "Robby", "Michael"],
-    answer: "  Kyle"
+    question: "What is the name of the first ewok Princess Leia encounters on Endoor?",
+    options: ["Teek", "Wicket", "Weedee", "Nubnub"],
+    answer: "Wicket"
   },
   {
     uniqueID: 3,
     question: "What is the name the spaceship captained by Han Solo?",
     options: ["X-wing", "Y-wing", "Milenium Eagle", "Milenium Falcon"],
-    answer: "  Milenium Falcon"
+    answer: "Milenium Falcon"
+  },
+  {
+    uniqueID: 4,
+    question: "What is the name of the Bounty Hunter that captures Han Solo",
+    options: ["Jango Fett", "IG-88", "Boba Fett"],
+    answer: "Boba Fett"
+  },
+  {
+    uniqueID: 5,
+    question: "What is the name the spaceship captained by Han Solo?",
+    options: ["X-wing", "Y-wing", "Milenium Eagle", "Milenium Falcon"],
+    answer: "Milenium Falcon"
+  },
+  {
+    uniqueID: 6,
+    question: "What is the name the spaceship captained by Han Solo?",
+    options: ["X-wing", "Y-wing", "Milenium Eagle", "Milenium Falcon"],
+    answer: "Milenium Falcon"
   }
 ];
